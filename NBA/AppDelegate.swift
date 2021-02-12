a7f2287621f956d7c47019e53004b5908ca94495
Gabriele Rulli <rulligabri.95@gmail.com>
2021-02-12 08:57:24 +0100

    Get all players from API and save the list to the local storage
    - Get list of players filtered by team id in the team detail interaction

--- NBA/AppDelegate.swift
+++ NBA/AppDelegate.swift
@@ -1,8 +1,78 @@
 import UIKit
+import Alamofire
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
+    var numberOfPlayersHttpCalls: Int = 0
+    let userDefaults = UserDefaults.standard
+    
+    func getPlayers(pageNumber: Int = 0, completionHandler: @escaping () -> Void) {
+        AF.request("\(HttpRequestManager.baseUrl())/players?page=\(pageNumber)&per_page=100", headers: HttpRequestManager.headers())
+            .validate()
+            .responseJSON { (response: AFDataResponse<Any>) in
+                var players: [Player] = []
+
+                switch response.result {
+                    case .success(let value):
+                        if let JSON = value as? [String: Any] {
+                            if self.numberOfPlayersHttpCalls == 0 {
+                                // Check total pages number returned by API and save it in order to get all pages and retrive every players
+                                let data = JSON["meta"] as! [String: Any]
+                                self.numberOfPlayersHttpCalls = data["total_pages"] as! Int
+                            } else {
+                                let data = JSON["data"] as! NSArray
+                                // Map results to an array of Player
+                                players = data.map({ (player) -> Player in
+                                    return Player(data: player as! [String : Any])
+                                })
+                                
+                                do {
+                                    var savedPlayersList: [Player] = []
+                                    
+                                    // Get players data from local storage
+                                    if let playersData = self.userDefaults.data(forKey: "players") {
+                                        do {
+                                            // Add players from API to the list of the players saved locally
+                                            savedPlayersList = try JSONDecoder().decode([Player].self, from: playersData)
+                                            players.append(contentsOf: savedPlayersList)
+                                        } catch {
+                                           print(error)
+                                        }
+                                    }
+                                    
+                                    // Encode players list and save it to local storage
+                                    let encodedData = try JSONEncoder().encode(players)
+                                    self.userDefaults.set(encodedData, forKey: "players")
+                                    self.userDefaults.synchronize()
+                                } catch (_) {
+                                    print("error")
+                                }
+                            }
+                            
+                            completionHandler()
+                        }
+                case .failure(_):
+                    completionHandler()
+                    break
+                    // Do nothing
+                }
+            }
+    }
+    
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        // Reset players list saved locally
+        self.userDefaults.set([], forKey: "players")
+        let queue = DispatchQueue(label: "com.app.concurrentQueue", attributes: .concurrent)
+        DispatchQueue.global(qos: .background).async {
+            self.getPlayers() {
+                for n in 1...self.numberOfPlayersHttpCalls {
+                    queue.async {
+                        self.getPlayers(pageNumber: n) {}
+                    }
+                }
+            }
+        }
+
         // Override point for customization after application launch.
         return true
     }
--- NBA/Scenes/TeamDetail/TeamDetailInteractor.swift
+++ NBA/Scenes/TeamDetail/TeamDetailInteractor.swift
@@ -17,6 +17,17 @@ class TeamDetailInteractor: TeamDetailBusinessLogic
     var teamPlayers: [Player] = []
     var success: Bool = true
     
+    if let playersData = UserDefaults.standard.data(forKey: "players") {
+        do {
+            let allPlayers = try JSONDecoder().decode([Player].self, from: playersData)
+            teamPlayers = allPlayers.filter {
+                $0.teamId == request.teamId
+            }
+        } catch {
+           success = false
+        }
+    }
+    
     let response = TeamDetail.Response(success: success, players: teamPlayers)
     self.presenter?.presentSomething(response: response)
   }
